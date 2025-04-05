64aaf8068f20aab3ee4b1470b1a254c99c4e4e3f
Pradeep <webdevpradeep0101@gmail.com>
2025-04-05 03:59:14 -1200

    responsive design

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -17,12 +17,12 @@ const Home = () => {
         <img
           src="src/assets/Durga_Mata.png"
           alt="maa durga ki photo"
-          className=" contrast-150 rounded-2xl shadow-2xl shadow-black hover:scale-150 animate-pulse brightness-110 duration-75 ease-in-out h-96 w-80 object-contain  md:h-72 md:w-72  md:object-contain md:bg-gradient-to-t md:to-pink-400 md:from-red-600 md:rounded-lg  md:shadow-2xl md:shadow-black md:brightness-90  md:transition md:ease-in-out md:delay-150  md:hover:scale-140 md:duration-[0.5s] md:hover:animate-none md:animate-ping  "
+          className="  contrast-150 rounded-2xl shadow-2xl shadow-black hover:scale-150 animate-pulse brightness-110 duration-75 ease-in-out h-65 w-60 object-contain  md:h-72 md:w-72  md:object-contain md:bg-gradient-to-t md:to-pink-400 md:from-red-600 md:rounded-lg  md:shadow-2xl md:shadow-black md:brightness-90  md:transition md:ease-in-out md:delay-150  md:hover:scale-140 md:duration-[0.5s] md:hover:animate-none md:animate-ping  "
         />
       </section>
 
       <form
-        className=" w-[100%] flex justify-center items-center mt-15 gap-10 md:fixed md:bottom-10 md:flex md:justify-center  md:w-[100%] md:gap-5"
+        className=" w-[100%] flex flex-col justify-center items-center mt-10 gap-5 md:fixed md:bottom-10 md:flex md:justify-center  md:w-[100%] md:gap-5"
         onSubmit={handler}
       >
         <input
@@ -31,7 +31,7 @@ const Home = () => {
           className="bg-red-400 text-2xl caret-black  text-white p-2 pl-5 rounded-xl outline-none md:bg-red-400 md:text-white md:p-2 md:rounded-md  md:text-2xl md:caret-pink-500 md:outline-2xl "
         />
         <button
-          className={` flex justify-center items-center p-2 text-2xl rounded active:active:bg-red-400  transition-all duration-[0.2s] ease-in-out md:w-20 md:text-2xl  md:rounded md:text-white md:active:bg-red-400  md:transition-all md:duration-[0.2s] md:ease-in-out  ${
+          className={` text-center w-[120px] h-[40px] text-2xl rounded active:active:bg-red-400  transition-all duration-[0.2s] ease-in-out md:w-20 md:text-2xl  md:rounded md:text-white md:active:bg-red-400  md:transition-all md:duration-[0.2s] md:ease-in-out  ${
             inputValue
               ? "bg-green-600 md:bg-green-600"
               : "bg-red-600 md:bg-red-600"
